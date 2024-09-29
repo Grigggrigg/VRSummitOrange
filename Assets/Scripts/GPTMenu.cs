4ce7922ae6e6af570e00d0378f20ccb387b39b78
Deni Ismailov <me@deni-ismailov.de>
2024-09-29 15:53:20 +0200

    feat: add chatbot voice

--- Assets/Scripts/GPTMenu.cs
+++ Assets/Scripts/GPTMenu.cs
@@ -4,17 +4,58 @@ using OpenAI_API.Chat;
 using OpenAI_API.Models;
 using System.Threading.Tasks;
 using System;
-using Cysharp.Threading.Tasks; 
+using Cysharp.Threading.Tasks;
+using System.Net.Http;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 public class GPTMenu : MonoBehaviour
 {
-    [SerializeField] private string openAiApiKey;
-    private OpenAIAPI api;
-    private AudioClip audioClip;
-    private const int sampleRate = 44100;
+    [SerializeField]
+    private AudioClip introduction;
+    [SerializeField]
+    private AudioClip meditation;
+    [SerializeField]
+    private AudioClip alcohol;
+    [SerializeField]
+    private AudioClip empathy;
 
-    public void Start()
+    [SerializeField]
+    private string meditationScene;
+    [SerializeField] private string alcoholScene;
+    [SerializeField] private string empathyScene;
+
+ [SerializeField]
+    private AudioSource audioSource;
+
+    public async void Start()
     {
-        api = new OpenAIAPI(openAiApiKey);
+        await UniTask.Delay(TimeSpan.FromSeconds(3), ignoreTimeScale: false);
+        await PlayAudio(introduction);
+    }
+
+    public async void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            await PlayAudio(meditation);
+            SceneManager.LoadScene(meditationScene);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            await PlayAudio(alcohol);
+            SceneManager.LoadScene(alcoholScene);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            await PlayAudio(empathy);
+            SceneManager.LoadScene(empathyScene);
+        }
+    }
+
+    private async Task PlayAudio(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+        await UniTask.Delay(TimeSpan.FromSeconds(clip.length + 1), ignoreTimeScale: false);
     }
 }
